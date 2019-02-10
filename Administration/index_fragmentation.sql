16b7f1c564dd60d12def029417ef254c4a8e832b
Rafael Lima <ls.rafael@icloud.com>
2019-02-10 11:40:07 -0200

    Update index_fragmentation.sql

--- Administration/index_fragmentation.sql
+++ Administration/index_fragmentation.sql
@@ -19,7 +19,7 @@ AND ind.index_id = indexstats.index_id
 WHERE indexstats.avg_fragmentation_in_percent > 30 
 ORDER BY indexstats.avg_fragmentation_in_percent DESC
 
--- Reorganiza os indices com menos de 30% de fragmentacao
+-- Criacao do script que reorganiza os indices com menos de 30% de fragmentacao
 select 'ALTER INDEX [' + ind.name + '] ON ['+ OBJECT_SCHEMA_NAME(ind.object_id) +'].[' + OBJECT_NAME(ind.OBJECT_ID)+ '] REORGANIZE  WITH ( LOB_COMPACTION = ON )'
 FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) indexstats 
 INNER JOIN sys.indexes ind  
